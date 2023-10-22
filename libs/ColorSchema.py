a330ced87b7dc025b7e3837af7e80430bfa9bdda
Niptlox <ggnipt@ya.ru>
2023-10-22 23:06:50 +0500

    start functions #unstableVer

---
+++ libs/ColorSchema.py
@@ -0,0 +1,13 @@
+import pygame as pg
+
+class ColorSchema:
+    grid_bg_color = "#9badb7"
+    grid_line_color = "#847e87"
+    tools_menu_font_1 = pg.font.SysFont("Roboto", 25)
+    tools_menu_font_labels = pg.font.SysFont("Roboto", 25)
+    tools_menu_font_1_color = "#FFFFFF"
+    toolsmenu_bg_color = "#4B5563"
+    toolsmenu_border_color = "#000000"
+    toolsmenu_active_cell_color = "#FFFFFF"
+    toolsmenu_vscroll_step = 25
+    highlighting_color = "#2e70ff"
